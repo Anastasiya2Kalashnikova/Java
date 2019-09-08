03ddf36d8c7466f4a855fde35b5ac5445d66ac37
Anastasiya <ak1059870@gmail.com>
2019-09-08 12:53:20 +0300

    Настроен гит игнор

--- DZ_from_books/Java_Tom1/Java_p052project/src/sample/Controller.java
+++ DZ_from_books/Java_Tom1/Java_p052project/src/sample/Controller.java
@@ -3,6 +3,6 @@ package sample;
 public class Controller {
     public static void main(String[] args)
     {
-        System.out.println("We will not use 'Hello, World! ");
+        System.out.println("We will not use 'Hello, World! Wow");
 }
 }
